52f55b48a0311aacfd1eb9f54cce6c3b31b5ea62
threadkind <threadkind@gmail.com>
2018-08-27 00:05:28 -0500

    feat: Add service worker functionality to cache resources

--- sw.js
+++ sw.js
@@ -1,10 +1,52 @@
-self.addEventListener('install',
+let staticCache = restaurant_reviews_v1
+
+self.addEventListener('fetch',
 event => {
-	console.log(`Successful`);
+	console.log(event.request.url);
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => {
+			if (response) {
+				return response;}
+			else {
+				return fetch(event.request);
+			}
+
+		})
+		);
 }
 );
 
-self.addEventListener('activate',
-	event => {
-		console.log('Active');
-	})
+self.addEventListener('install',
+event => {
+	console.log(`Successful`);
+	event.waitUntil(
+    caches.open(staticCache).then(function(cache) {
+      return cache.addAll(
+        [
+          '/css/styles.css',
+          '/data/restaurants.json',
+          '/img/1.jpg',
+          '/img/2.jpg',
+          '/img/3.jpg',
+          '/img/4.jpg',
+          '/img/5.jpg',
+          '/img/6.jpg',
+          '/img/7.jpg',
+          '/img/8.jpg',
+          '/img/9.jpg',
+          '/img/10.jpg',
+          '/js/dbhelper.js',
+          '/js/main.js',
+          '/js/restaurant_info.js',
+          '/index.html',
+          '/restaurant.html',
+          '/sw.js'
+        ]
+      );
+    })
+  );
+}
+);
+
+
